53df8ed17b1a58bbc2b1b03d7bf6f87072ddfe16
Mahalingam1592 <mahalingamsubramniyan@gmail.com>
2021-07-24 10:50:21 +0530

    hello

---
+++ src/org/bike/Hornet.java
@@ -0,0 +1,13 @@
+package org.bike;
+
+public class Hornet {
+
+	public void cost() {
+		System.out.println("cost is 90000");
+
+	}
+	public void speed() {
+		System.out.println("speed is 160");
+
+	}
+}
---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,21 @@
+package org.bike;
+
+public class Ktm extends Hornet {
+public void cost() {
+System.out.println("speed is 160");
+
+}
+
+public void speed() {
+	System.out.println("speed is 200");
+}
+public static void main(String[] args) {
+	Ktm c=new Ktm();
+	c.cost();
+	c.speed();
+	
+	
+	
+}
+
+}
